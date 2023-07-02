a60f0dd3ff00ad14a4506dbd4490442ed495c8e3
nataliya-nataliya <nataliya.chest@gmail.com>
2023-07-02 20:45:58 +0400

    2. Candidate Repository. Storage of candidates. [#504861]

--- src/main/java/ru/job4j/dreamjob/service/SimpleCandidateService.java
+++ src/main/java/ru/job4j/dreamjob/service/SimpleCandidateService.java
@@ -15,9 +15,9 @@ public class SimpleCandidateService implements CandidateService {
     private final CandidateRepository candidateRepository;
     private final FileService fileService;
 
-    private SimpleCandidateService(CandidateRepository candidateRepository,
+    private SimpleCandidateService(CandidateRepository sql2oCandidateRepository,
                                    FileService fileService) {
-        this.candidateRepository = candidateRepository;
+        this.candidateRepository = sql2oCandidateRepository;
         this.fileService = fileService;
     }
 
